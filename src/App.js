eae851628eebab77c494a5b9dee74a673e049e06
gamenic-biren <gamenic.biren@gmail.com>
2020-03-10 14:54:46 +0545

    initial commit

---
+++ src/App.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      newItem: "",
+      list: []
+    }
+  }
+
+  updateInput(key, value){
+    //update react state
+    this.setState({
+      [key]:value
+    })
+  }
+
+  addItem(){
+    //create item with unique id
+    const newItem={
+      id: 1 + Math.random(),
+      value: this.state.newItem.slice()
+    };
+
+    //copy of current list of items
+    const list =[...this.state.list];
+
+    //add new items to list
+    list.push(newItem);
+
+    //update state with new list and reset newItem input
+    this.setState({
+      list,
+      newItem:""
+    })
+  }
+
+  deleteItem(id){
+    //copy of current list of items
+    const list = [...this.state.list];
+
+    //filter out item being deleted
+    const updatedList = list.filter(item => item.id !==id);
+
+    this.setState({list:updatedList});
+  }
+
+  render(){
+    return ( 
+      <div className = "App" >
+        <div>
+          Add an Item ...
+          <br />
+          <input 
+            type = "text"
+            placeholder = " Type item here..."
+            value = {
+            this.state.newItem}
+            onChange = {e => this.updateInput("newItem", e.target.value)}
+          /> 
+          <button id="b1"
+            onClick = {() => this.addItem()} 
+          >
+            Add 
+          </button>
+            <ul>
+              {this.state.list.map(item =>{
+                return(
+                  <li key={item.id}>
+                    {item.value}
+                    <button id="b2"
+                      onClick={() => this.deleteItem(item.id)}
+
+                    >
+                    X
+                    </button>
+                  </li>
+                )
+              })}
+              </ul> 
+        </div> 
+      </div>
+    );
+  }
+}
+
+export default App;
